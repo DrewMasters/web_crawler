6fb8e44790c4693646abf2009b0f347408db0497
Drew Masters <dmaster1@lap167.local>
2017-05-02 17:00:31 -0400

    UI and threading added

--- web_crawler.py
+++ web_crawler.py
@@ -2,18 +2,18 @@ import urllib
 import re
 import HTMLParser
 import Tkinter as tk
+import ScrolledText as tkst
 import Queue
 import operator
+import threading
 
-visit_links = Queue.Queue()
-visited = {}
-print("created visit_links and visited")
-state = 0
-print("created state")
-
-url = "https://docs.python.org/2/library/re.html"
-url = "http://google.com"
-visit_links.put(url)
+#visit_links = Queue.Queue()
+#visited = {}
+#state = 0
+#top = tk.Tk()
+#url = "https://docs.python.org/2/library/re.html"
+#url = "http://google.com"
+#visit_links.put(url)
 
 def url_visit(url):
 	#get domain name of url
@@ -42,9 +42,49 @@ def url_visit(url):
 
 def print_visited():
 	sorted_visited = sorted(visited.items(),key=operator.itemgetter(1),reverse=True)
+	s = ""
 	for i in sorted_visited:
-		print(i)
+		s = s + str(i) +"\n"
+	T1.insert('insert',s)
 
-while not visit_links.empty():
-	url_visit(visit_links.get())
-	print_visited()
+def process():
+	global state
+
+	if state ==0:
+		url = E1.get()
+		visit_links.put(url)
+		state = 1
+		t = threading.Thread(target=thread_url)
+		t.daemon = True
+		t.start()
+	elif state ==1:
+		state = 2
+	elif state == 2:
+		state =1
+	elif state == 3:
+		state = 1
+		
+def thread_url():
+	global state
+	global visit_links
+
+	while not visit_links.empty():
+		if state == 1:
+			T1.insert('insert',"")
+			url_visit(visit_links.get())
+		elif state == 2:
+			print_visited()
+			state =3
+	
+visit_links = Queue.Queue()
+visited = {}
+state = 0
+top = tk.Tk()
+#visit_links.put(url)
+B1 = tk.Button(top, text="start/pause/resume",command=process)
+E1 = tk.Entry(top,bd=5)
+T1 = tkst.ScrolledText(top)
+B1.pack()
+E1.pack()
+T1.pack()
+top.mainloop()
