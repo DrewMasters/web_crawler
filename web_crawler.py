caff2a8ed35ab60c54203cd7d0b9159849f5bed7
Drew Masters <dmaster1@lap167.local>
2017-05-02 16:07:14 -0400

    just need to add UI

--- web_crawler.py
+++ web_crawler.py
@@ -3,10 +3,16 @@ import re
 import HTMLParser
 import Tkinter as tk
 import Queue
+import operator
 
 visit_links = Queue.Queue()
+visited = {}
+print("created visit_links and visited")
+state = 0
+print("created state")
 
 url = "https://docs.python.org/2/library/re.html"
+url = "http://google.com"
 visit_links.put(url)
 
 def url_visit(url):
@@ -22,10 +28,23 @@ def url_visit(url):
 	html = handle.read()
 	
 	links = re.findall('"((http|ftp)s?://.*?)"', html)
+	count = 0
 	for t in links:
 		turl = t[0]
 		if domain != re.search('.+://[\w\.]*',turl).group(0):
-			visit_links.put(turl)
+			if turl in visited:
+				visited[turl]+=1
+			else:
+				visited[turl]=1
+				visit_links.put(turl)
+				count +=1
+	print("Found " + str(count) + " remote URLs on " + str(url))
+
+def print_visited():
+	sorted_visited = sorted(visited.items(),key=operator.itemgetter(1),reverse=True)
+	for i in sorted_visited:
+		print(i)
 
 while not visit_links.empty():
 	url_visit(visit_links.get())
+	print_visited()
